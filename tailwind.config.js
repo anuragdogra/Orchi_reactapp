b31ef7500eea8a3302082d3301d23dd0888fb59f
anuragdogra <44124122+anuragdogra@users.noreply.github.com>
2024-05-30 10:57:28 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,25 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      'animation': {
-            'text':'text 5s ease infinite',
+      screens: {
+        xs: '200px',
+      },
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
         },
-        'keyframes': {
-            'text': {
-                '0%, 100%': {
-                   'background-size':'200% 200%',
-                    'background-position': 'left center'
-                },
-                '50%': {
-                   'background-size':'200% 200%',
-                    'background-position': 'right center'
-                }
-            },
-        }
+      },
     },
   },
   plugins: [],
 }
-
